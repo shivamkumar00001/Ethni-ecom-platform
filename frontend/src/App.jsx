fdc2b45f3830f89ca78b1f1f54f522700891efd1
Ananya <justdiscussanything@gmail.com>
2025-05-01 01:46:14 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Cart from './components/Front-End-components/cart'
-import DashBoard from './components/Front-End-components/dash-board'
-import Likes from './components/Front-End-components/likes'
-import LoginSignInPage from './components/Front-End-components/login-signIn-page'
-import PaymentGateway from './components/Front-End-components/paymentGateway'
-import ProductPage from './components/Front-End-components/product-page'
-
-// function App() {
- 
-  // return (
-  //   <>
-  //    <Cart/>
-  //    <DashBoard/>
-  //    <Likes/>
-  //    <LoginSignInPage/>
-  //    <PaymentGateway/>
-  //    <ProductPage/>
-  //   </>
-  // )
-// }
 import ProductList from "./components/ProductList";
-import ProductDetails from './components/productDetails'
 
+const App = () => {
+  const fetch = ()=>{
+    fetch("http://")
+  }
+  return <ProductPage />;
+};
 
-function App() {
-  return (
-    <div>
-      <h1>Ethnic Ecom Platform</h1>
-      <ProductList/>
-    </div>
-  );
-}
-
-export default App
+export default App;
