1415e59d4c90f79156a56fce6206cd4849a9edec
Ananya <justdiscussanything@gmail.com>
2025-05-01 07:42:13 +0530

    sign-up-page-design-complete

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,14 +1,35 @@
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { NavLink } from "react-router";
 import "../styles/auth.css";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import logo from "../assets/logo.png";
+import { one, two, three, four, five, six } from "../assets/images.js";
 
 const SignUp = () => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const [name, setName] = useState("");
+  const imgs = useRef([one, two, three, four, five, six]);
+  const swiper = useRef();
+  useEffect(() => {
+    let index = 1;
+
+    setInterval(() => {
+      for (let i = 0; i < imgs.current.length; i++) {
+        document.getElementsByClassName("imgs")[i].style.opacity =
+          i != index ? 0 : 1;
+      }
+
+      index = (index + 1) % imgs.current.length;
+    }, 6000);
+  }, []);
   return (
     <>
       <div id="container">
+        <div id="logo">
+          <img src={logo} height="100%" width="100%" />
+          <span id="company-name">Ethinica</span>
+        </div>
         <div id="left-side">
           <div id="form-heading">
             <div id="main-heading">Create Account</div>
@@ -19,7 +40,13 @@ const SignUp = () => {
           <form id="form">
             <label>
               Full Name
-              <input type="text" placeholder="Enter your full name" required />
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </label>
             <label>
               Email
@@ -49,10 +76,26 @@ const SignUp = () => {
           </form>
           <div id="form-footer">
             <span>Alread have an account?</span>
-            <NavLink id="form-footer-link">Sign in</NavLink>
+            <NavLink id="form-footer-link" to={"/login"}>
+              Sign in
+            </NavLink>
           </div>
         </div>
-        <div id="right-side">right</div>
+        <div id="right-side">
+          <div id="green-overlay"></div>
+          <div id="swiper" ref={swiper}>
+            {imgs.current.map((img, index) => (
+              <div
+                className="imgs"
+                key={index}
+                style={{
+                  backgroundImage: `url(${img})`,
+                  opacity: index == 0 ? 1 : 0,
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
